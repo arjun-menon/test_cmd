18e7557d5bfce8a1b5c0e82e79e28be66a7b34e9
Arjun G. Menon <contact@arjungmenon.com>
2016-11-03 17:37:15 -0400

    Update setup.py w/ download_url

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 
 from setuptools import setup
 
+version = '1.0.0'
+repo_url = 'https://github.com/arjun-menon/test_cmd'
+download_url = repo_url + '/tarball/' + version
+
 setup(name='test_cmd',
-      version='1.0',
+      version=version,
       description='Tool for black-box testing command-line programs using STDIN, STDOUT and STDERR',
       long_description=open('README.rst').read(),
-      url='https://github.com/arjun-menon/test_cmd',
+      url=repo_url,
+      download_url=download_url,
       author='Arjun G. Menon',
       author_email='contact@arjungmenon.com',
       keywords='command line terminal functional black box testing arguments stdin stdout stderr',
