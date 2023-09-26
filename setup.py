534355c82eb47eb0855c3f79beed0f692e6f96b1
Arjun G. Menon <contact@arjungmenon.com>
2023-09-26 19:36:06 +0400

    Minor update to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from setuptools import setup
 
 version = '2.0.4'
+
 repo_url = 'https://github.com/arjun-menon/test_cmd'
-download_url = repo_url + '/tarball/' + version
+download_url = "%s/archive/v%s.tar.gz" % (repo_url, version)
 
 setup(name='test_cmd',
       version=version,
@@ -15,7 +16,7 @@ setup(name='test_cmd',
       author='Arjun G. Menon',
       author_email='contact@arjungmenon.com',
       keywords='command line terminal functional black box testing arguments stdin stdout stderr',
-      license='Apache',
+      license='Apache-2.0',
       py_modules=['test_cmd'],
       entry_points = {
         'console_scripts': ['test_cmd=test_cmd:main'],
